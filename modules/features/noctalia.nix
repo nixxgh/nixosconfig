568895a86075c23697309580009144efd971ad92
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-08-29 23:49:42 +0530

    changed parser from json to toml for noctalia

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -6,9 +6,7 @@
       inherit pkgs;
       # override the default package with the new v5 beta flake
       package = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
-      settings = 
-        (builtins.fromJSON
-          (builtins.readFile ./.noctalia.json)).settings;
+      settings = builtins.fromTOML (builtins.readFile ./.noctalia-config.toml);
       
     };
 
