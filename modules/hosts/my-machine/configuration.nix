6b7b2bebb0eb74f1373964b6b5780b5b33883abf
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-08-30 14:20:11 +0530

    formatted ncupdate alias code visually

--- modules/hosts/my-machine/configuration.nix
+++ modules/hosts/my-machine/configuration.nix
@@ -9,7 +9,8 @@
     # Environment Aliases
     environment.shellAliases = {
     rebuild = "sudo nixos-rebuild switch --flake /home/nixx/myNixOS#myMachine";
-    ncupdate = "nix run nixpkgs#noctalia -- config export > ~/myNixOS/modules/features/.noctalia-config.toml && echo 'stage and commit myNixOS, to keep tree clean!'";
+    ncupdate = "nix run nixpkgs#noctalia -- config export > ~/myNixOS/modules/features/.noctalia-config.toml " +
+               "&& echo 'stage and commit myNixOS, to keep tree clean!'";
     };
 
     # Bootloader.
