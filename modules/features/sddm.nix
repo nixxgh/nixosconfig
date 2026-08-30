a5203043fd2231ec26e55f4b24f8a0b0d14bf777
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-08-30 15:01:34 +0530

    trying to add touch support in sddm wayland

--- modules/features/sddm.nix
+++ modules/features/sddm.nix
@@ -3,7 +3,15 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+
+      settings = {
+        Wayland = {
+          # Tell SDDM to launch Weston using a config file generated in the Nix store
+          CompositorCommand = "weston --shell=fullscreen-shell.so --config ${pkgs.writeText \"sddm-weston.ini\" ''
+            [libinput]
+            enable-tap=true
+          ''}";
+        };
     };
-      #package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
   };
 } 
