52269574f9947114e89918ecb093f4939820cd02
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-08-30 15:12:04 +0530

    use non wayland sddm for mouse support removed wayland settings from sddm.nix

--- modules/features/sddm.nix
+++ modules/features/sddm.nix
@@ -2,17 +2,6 @@
   flake.nixosModules.sddm = { pkgs, lib, ... }: {
     services.displayManager.sddm = {
       enable = true;
-      wayland.enable = true;
-
-      settings = {
-        Wayland = {
-          # Notice how it is just "sddm-weston.ini" now, without the backslashes!
-          CompositorCommand = "weston --shell=fullscreen-shell.so --config ${pkgs.writeText "sddm-weston.ini" ''
-            [libinput]
-            enable-tap=true
-          ''}";
-        };
-      };   
     };
   };
 } 
