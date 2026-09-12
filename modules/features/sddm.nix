f51cb2afacc7b7fbe88c53ec40dfb15af27da663
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-09-12 19:41:48 +0530

    no comment by user

--- modules/features/sddm.nix
+++ modules/features/sddm.nix
@@ -1,6 +1,7 @@
 { self, inputs, ... }: { 
   flake.nixosModules.sddm = { pkgs, lib, ... }: {
     services.xserver.enable = true;
+    services.xserver.desktopManager.xterm.enable = false;
 
     services.displayManager.sddm = {
       enable = true;
