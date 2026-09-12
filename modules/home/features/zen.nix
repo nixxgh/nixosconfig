53629dbf7f0aad717075721c744a06a21bdd293a
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-09-12 17:35:07 +0530

    feat(zen): set Zen Browser as declarative default browser

--- modules/home/features/zen.nix
+++ modules/home/features/zen.nix
@@ -1,11 +1,19 @@
 { self, inputs, ... }: {
-  flake.homeModules.zen = { config, pkgs, ... }: {
+  flake.homeModules.zen = { config, pkgs, lib, ... }: {
     imports = [
       inputs.zen-browser.homeModules.default
     ];
 
     programs.zen-browser = {
       enable = true;
+      setAsDefaultBrowser = true;
+    };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/plain" = lib.mkForce "nvim.desktop";
+      };
     };
   };
 }
