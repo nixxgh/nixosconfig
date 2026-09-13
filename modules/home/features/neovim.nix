566e3b6920567a3c90e20e7d12c11f0ad388d3e0
nixxgh <275239900+nixxgh@users.noreply.github.com>
2026-09-13 23:18:13 +0530

    feat(neovim): update format keymap to <leader>f (Space + f)

--- modules/home/features/neovim.nix
+++ modules/home/features/neovim.nix
@@ -68,8 +68,8 @@
           },
         })
 
-        -- Format keymap: <leader>cf (Code Format)
-        vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
+        -- Format keymap: <leader>f (Space + f)
+        vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
           require('conform').format({
             lsp_fallback = true,
             async = false,
